26fa6593aeb04ebd33397751a8efff88bdc02f09
Teru3301 <116116906+Teru3301@users.noreply.github.com>
2023-09-21 15:50:12 +0300

    Update main.cpp

--- Contest-2023-09-12/18/main.cpp
+++ Contest-2023-09-12/18/main.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+#include <string>
 
+int main()
+{
+	long long n;
+	std::string word = "";
+	std::cin >> n;
+	while (n > 0)
+	{
+		n--;
+		word = char((n % 26) + 65) + word;
+		n = n / 26;
+	}
+
+	std::cout << word;
+
+	return 0;
+}
