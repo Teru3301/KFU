9010a6b84adf7e7f944a02cdafd7154f1e20136d
Teru3301 <116116906+Teru3301@users.noreply.github.com>
2023-09-21 15:55:44 +0300

    Update main.cpp

--- Contest-2023-09-12/03/main.cpp
+++ Contest-2023-09-12/03/main.cpp
@@ -1,1 +1,14 @@
+#include <iostream>
+#include <iomanip>
 
+int main()
+{
+	double a;
+	double b;
+
+	std::cin >> a;
+	std::cin >> b;
+	std::cout << std::setprecision(32) << a / b << std::endl;
+
+	return 0;
+}
