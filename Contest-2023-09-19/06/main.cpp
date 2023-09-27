4b5070020d76fae75d69a8e3434b7e0acce7401f
Teru3301 <116116906+Teru3301@users.noreply.github.com>
2023-09-27 20:45:54 +0300

    Update main.cpp

--- Contest-2023-09-19/06/main.cpp
+++ Contest-2023-09-19/06/main.cpp
@@ -1,1 +1,39 @@
+Student make_student(std::string line)
+{
+    Student ns; // new student
+    ns.name = line;
+    ns.group = line;
 
+    ns.name.replace(ns.name.find(';'), ns.name.length() - ns.name.find(';'), "");
+    ns.group.replace(0, ns.group.find(';')+1, "");
+
+    return ns;
+}
+
+bool is_upper(Student st1, Student st2)
+{
+    if (st1.group == st2.group)
+    {
+        return st1.name < st2.name;
+    }
+
+    return st1.group < st2.group;
+}
+
+void print(std::vector<Student> students)
+{
+    if (students.size() == 0)
+    {
+        std::cout << "Empty list!";
+        return;
+    }
+
+    std::cout << students[0].group << std::endl;
+    std::cout << "- " << students[0].name << std::endl;
+
+    for (int i = 1; i < students.size(); i++)
+    {
+        if (students[i].group != students[i - 1].group) std::cout << students[i].group << std::endl;
+        std::cout << "- " << students[i].name << std::endl;
+    }
+}
