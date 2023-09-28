6d3ce555d822d293c76a1aa75e4ae2f094ae1f57
Teru3301 <116116906+Teru3301@users.noreply.github.com>
2023-09-28 22:01:51 +0300

    Update main.go

--- Contest-2023-09-19/10/main.go
+++ Contest-2023-09-19/10/main.go
@@ -1,1 +1,25 @@
+func shift(data []int, steps int) {
 
+	steps *= -1
+
+	for steps < 0 {
+		steps += 10
+	}
+
+	for steps > 9 {
+		steps -= 10
+	}
+
+	var tmp [10]int
+	for i := 0; i < 10; i++ {
+		tmp[i] = data[i]
+	}
+
+	for i := range data {
+		if i+steps >= 10 {
+			data[i] = tmp[i+steps-10]
+		} else {
+			data[i] = tmp[i+steps]
+		}
+	}
+}
