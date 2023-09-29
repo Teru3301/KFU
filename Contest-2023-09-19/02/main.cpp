c6d4c737ea17776a09342c3b0a511a4415dbeea2
Teru3301 <116116906+Teru3301@users.noreply.github.com>
2023-09-29 14:11:26 +0300

    Update main.cpp

--- Contest-2023-09-19/02/main.cpp
+++ Contest-2023-09-19/02/main.cpp
@@ -1,3 +1,8 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+// Ваш код будет вставлен сюда
 #define debug false
 
 void way(std::vector<std::string> &maze, int y, int x)
@@ -105,4 +110,16 @@ bool is_can_exit_from_maze(std::vector<std::string> maze, int y, int x)
             continue;
         }
     }
+}
+
+
+int main(){
+    int row, col, rows, cols;
+    std::cin >> row >> col >> rows >> cols;
+    std::cin.ignore(1); // Игнорируем перевод строки, застрявший в потоке ввода
+    
+    std::vector<std::string> maze(rows);
+    for(auto& line : maze) std::getline(std::cin, line);
+    
+    std::cout << (is_can_exit_from_maze(maze, row, col) ? "YES" : "NO") << std::endl;
 }
