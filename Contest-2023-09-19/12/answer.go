0c144f8618feb8c8e4d47a457248943440af64ef
Teru3301 <116116906+Teru3301@users.noreply.github.com>
2023-09-29 13:53:09 +0300

    Update answer.go

--- Contest-2023-09-19/12/answer.go
+++ Contest-2023-09-19/12/answer.go
@@ -1,1 +1,18 @@
+func fill(maze [][]int) {
+	for y := range maze {
+		for x := range maze[y] {
+			if maze[y][x] == 0 {
+				var xy = [8][2]int{{1, -1}, {1, 0}, {1, 1}, {0, 1}, {-1, 1}, {-1, 0}, {-1, -1}, {0, -1}}
 
+				for i := 0; i < 8; i++ {
+					if (y+xy[i][0] < 0) || (y+xy[i][0] > len(maze)-1) || (x+xy[i][1] < 0) || (x+xy[i][1] > len(maze[y])-1) {
+						continue
+					}
+					if maze[y+xy[i][0]][x+xy[i][1]] == -1 {
+						maze[y][x]++
+					}
+				}
+			}
+		}
+	}
+}
