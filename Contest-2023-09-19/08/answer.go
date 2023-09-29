6c581ab642e88b0406657ad63cc2c5fea2af5450
Teru3301 <116116906+Teru3301@users.noreply.github.com>
2023-09-29 14:01:28 +0300

    Update answer.go

--- Contest-2023-09-19/08/answer.go
+++ Contest-2023-09-19/08/answer.go
@@ -1,1 +1,22 @@
+import "strings"
 
+func isPalindrome(line string) bool {
+	line = strings.ToLower(line)
+
+	for i := 0; i < 'a'; i++ {
+		line = strings.ReplaceAll(line, (string)(i), "")
+	}
+    for i := 'z' + 1; i <= 255; i++ {
+		line = strings.ReplaceAll(line, (string)(i), "")
+	}
+
+	var ln = len(line) - 1
+
+	for i := 0; i < (len(line) / 2); i++ {
+		if line[i] != line[ln-i] {
+			return false
+		}
+	}
+
+	return true
+}
