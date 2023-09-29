ea1ac432aa51807b7ab4e197b85d7236a88e9130
Teru3301 <116116906+Teru3301@users.noreply.github.com>
2023-09-29 14:16:13 +0300

    Update main.cpp

--- Contest-2023-09-19/01/main.cpp
+++ Contest-2023-09-19/01/main.cpp
@@ -4,10 +4,8 @@
 bool is_prime(int num)
 {
 	if (num % 2 == 0) return false;
-	if (num % 3 == 0) return false;
-	if (num % 5 == 0) return false;
-	
-	for (int i = 7; i < num / i+1; i+=2)
+
+	for (int i = 3; i < num / (i-1); i+=2)
 	{
 		if (num % i == 0) return false;
 	}
