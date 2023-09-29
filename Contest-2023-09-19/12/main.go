6182eafcdb8f8ea4f25c5affdf11024e67daf71b
Teru3301 <116116906+Teru3301@users.noreply.github.com>
2023-09-29 13:54:13 +0300

    Update main.go

--- Contest-2023-09-19/12/main.go
+++ Contest-2023-09-19/12/main.go
@@ -1,1 +1,47 @@
+package main
 
+import "fmt"
+
+func fill(maze [][]int) {
+	for y := range maze {
+		for x := range maze[y] {
+			if maze[y][x] == 0 {
+				var xy = [8][2]int{{1, -1}, {1, 0}, {1, 1}, {0, 1}, {-1, 1}, {-1, 0}, {-1, -1}, {0, -1}}
+
+				for i := 0; i < 8; i++ {
+					if (y+xy[i][0] < 0) || (y+xy[i][0] > len(maze)-1) || (x+xy[i][1] < 0) || (x+xy[i][1] > len(maze[y])-1) {
+						continue
+					}
+					if maze[y+xy[i][0]][x+xy[i][1]] == -1 {
+						maze[y][x]++
+					}
+				}
+			}
+		}
+	}
+}
+
+func main() {
+	var rows, cols int
+	fmt.Scanf("%d %d\n", &rows, &cols)
+
+	// Создаём срез и заполняем его данными о расположении мин
+	maze := make([][]int, rows, rows)
+	for i := range maze {
+		maze[i] = make([]int, cols, cols)
+		for j := range maze[i] {
+			fmt.Scanf("%d", &maze[i][j])
+		}
+	}
+
+	// Заполняем игровое поле подсказками
+	fill(maze)
+
+	// Выводим на экран
+	for _, row := range maze {
+		for _, cell := range row {
+			fmt.Printf("%3d", cell)
+		}
+		fmt.Println()
+	}
+}
