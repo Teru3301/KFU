f59aecdb967a15871b175e17970a417a7a66baab
Teru3301 <116116906+Teru3301@users.noreply.github.com>
2023-09-29 13:55:56 +0300

    Update main.go

--- Contest-2023-09-19/11/main.go
+++ Contest-2023-09-19/11/main.go
@@ -1,3 +1,10 @@
+package main
+import "fmt"
+
+const rows int = 9
+const cols int = 9
+
+// Ваш код будет вставлен сюда
 func isValidSudoku(bord [rows][cols]int) bool {
 
 	for y := 0; y < rows; y++ {
@@ -20,4 +27,23 @@ func isValidSudoku(bord [rows][cols]int) bool {
 	}
 
 	return true
+}
+
+
+func main() {
+    var bord [rows][cols]int
+    
+    for row:=0; row < rows; row++ {
+        for col:=0; col < cols; col++ {
+            fmt.Scanf("%c", &bord[row][col])  // Считываем один символ
+            bord[row][col] -= '0'  // Чтобы из ASCII кода символа получить цифру
+        }
+        fmt.Scanf("\n")
+    }
+    
+    if isValidSudoku(bord){
+        fmt.Println("YES")
+    }else{
+        fmt.Println("NO")
+    }
 }
