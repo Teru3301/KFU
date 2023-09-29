6e34594577c2ae5374abb2956d6fa45746052ce0
Teru3301 <116116906+Teru3301@users.noreply.github.com>
2023-09-29 13:56:40 +0300

    Update answer.go

--- Contest-2023-09-19/11/answer.go
+++ Contest-2023-09-19/11/answer.go
@@ -1,1 +1,23 @@
+func isValidSudoku(bord [rows][cols]int) bool {
 
+	for y := 0; y < rows; y++ {
+		var sum int = 0
+		for x := 0; x < cols; x++ {
+			sum += bord[y][x]
+		}
+		if sum != 45 {
+			return false
+		}
+	}
+	for x := 0; x < cols; x++ {
+		var sum int = 0
+		for y := 0; y < rows; y++ {
+			sum += bord[y][x]
+		}
+		if sum != 45 {
+			return false
+		}
+	}
+
+	return true
+}
