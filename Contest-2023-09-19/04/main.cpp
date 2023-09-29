f4239d7548df974118d6c508b0df57dcd67ce265
Teru3301 <116116906+Teru3301@users.noreply.github.com>
2023-09-29 14:07:45 +0300

    Update main.cpp

--- Contest-2023-09-19/04/main.cpp
+++ Contest-2023-09-19/04/main.cpp
@@ -1,5 +1,8 @@
 // для того что бы код работал нужен с++17
+#include <iostream>
+#include <tuple>
 
+// Ваш код будет вставлен сюда
 std::tuple <int, int> reduce(int m, int n)
 {
     if (m == n) return std::tuple<int, int>(1, 1);
@@ -38,4 +41,19 @@ std::tuple <int, int, int> find_lcm(int n1, int n2)
     int c2 = lcm / n2;
 
     return std::tuple<int, int, int>(lcm, c1, c2);
+}
+
+
+int main() {
+    int m1, n1, m2, n2;
+    char _;
+    std::cin >> m1 >> _ >> n1
+             >> m2 >> _ >> n2;
+    std::tie(m1, n1) = reduce(m1, n1);
+    std::tie(m2, n2) = reduce(m2, n2);
+
+    auto[lcm, c1, c2] = find_lcm(n1, n2);
+    auto[m, n] = reduce(m1 * c1 + m2 * c2, lcm);
+
+    std::cout << m << '/' << n << std::endl;
 }
