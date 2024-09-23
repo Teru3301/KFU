1622a6f0024e365cfdfcc89ddb4a7e86f7b9cc0e
Teru3301 <116116906+Teru3301@users.noreply.github.com>
2024-09-23 22:38:27 +0300

    @Teru3301

--- Contests/Contest-2024-09-11/20/main.cpp
+++ Contests/Contest-2024-09-11/20/main.cpp
@@ -1,45 +1,18 @@
 #include <iostream>
 #include <string>
 #include <algorithm>
-using namespace std;
 
 int main()
 {
-	string num1;
-	string num2;
+	std::string num1;
+	std::string num2;
 
-	cin >> num1 >> num2;
+	std::cin >> num1 >> num2;
 
-	int* n1 = new int[num1.length()];
-	int* n2 = new int[num2.length()];
+	std::sort(num1.begin(), num1.end());
+	std::sort(num2.begin(), num2.end());
 
-	for (int i = 0; i < num1.length(); i++)
-		n1[i] = num1[i];
-
-	for (int i = 0; i < num2.length(); i++)
-		n2[i] = num2[i];
-
-	sort(n1, (n1 + num1.length()));
-	sort(n2, (n2 + num2.length()));
-
-	if (num1.length() == num2.length())
-	{
-		bool y = true;
-		for (int i = 0; i < num1.length(); i++)
-		{
-			if (n1[i] != n2[i])
-			{
-				cout << "NO";
-				y = !y;
-				break;
-			}
-		}
-		if (y) cout << "YES";
-	}
-	else
-	{
-		cout << "NO";
-	}
+	num1 == num2 ? std::cout << "YES" : std::cout << "NO";
 
 	return 0;
 }
