cc80820b8eb3a40d976b71756c5529401d512c4a
Teru3301 <timurrumiancev@gmail.com>
2024-11-05 17:18:59 +0200

    Исправлены не значительные ошибки

--- Contests/Contest-2024-10-16/04/classes.h
+++ Contests/Contest-2024-10-16/04/classes.h
@@ -1,3 +1,5 @@
+#pragma once
+
 class Employee {
 protected:
     std::string name, position;
--- Contests/Contest-2024-10-16/05/classes.h
+++ Contests/Contest-2024-10-16/05/classes.h
@@ -1,4 +1,7 @@
+#pragma once
+
 #include <queue>
+
 class OSUpdater : public IRunnable {
 public:
     void run() {
